ed90f1eac6efa4836249ddaff21439f387070293
Phaydara Vongsavanthong <stephen.mussel@gmail.com>
2021-08-28 13:28:59 -0500

    diplays employee object from array on table but each entry also lists previous entries

--- client.js
+++ client.js
@@ -23,13 +23,13 @@ function addEmployee() {
 
     // created new object
     let newEmployee = {newFirstName, newLastName, newID, newTitle, newAnnualSalary};
-    console.log(`all the inputs: 
-                ${newFirstName}, ${newLastName}, ${newID}, ${newTitle}, ${newAnnualSalary}`);
+    console.log(`all the inputs: ${newFirstName}, ${newLastName}, ${newID}, ${newTitle}, ${newAnnualSalary}`);
     employeeList.push(newEmployee); 
-
+    console.table(newEmployee);
+    
     // newEmployee object is in the array!
-    console.log(employeeList);
-
+    console.table(employeeList);
+    
     // clears inputs
     // newFirstName.val(''); !work, not a function? huh?
     $('#add-first-name').val(''); // works!
@@ -39,23 +39,24 @@ function addEmployee() {
     $('#add-annual-salary').val('');
 
     // call function to run it!
-    addEmployeeToTable();
+    displayEmployeeOnTable();
 }
 
-function addEmployeeToTable() {
-    console.log('in addEmployeeToTable');
+function displayEmployeeOnTable() {
+    console.log('in displayEmployeeOnTable');
     console.log(employeeList);
-    
-    // setting first object property in array to a variable
-    let newFirstName = employeeList[0].newFirstName;
-    let newLastName = employeeList[0].newLastName;
-    let newID = employeeList[0].newID;
-    let newTitle = employeeList[0].newTitle;
-    let newAnnualSalary = employeeList[0].newAnnualSalary;
-    let salaryNumber = parseInt(newAnnualSalary);
-    console.log(newFirstName, newLastName, newID, newTitle, salaryNumber);
 
-    let rowClass = '';
+    // need to loop this and append to table
+    for(i=0; i<employeeList.length; i++) {
+        let newFirstName = employeeList[i].newFirstName;
+        let newLastName = employeeList[i].newLastName;
+        let newID = employeeList[i].newID;
+        let newTitle = employeeList[i].newTitle;
+        let newAnnualSalary = employeeList[i].newAnnualSalary; // parseInt()
+        let salaryNumber = parseInt(newAnnualSalary);
+        console.log(newFirstName, newLastName, newID, newTitle, salaryNumber);
+
+        let rowClass = '';
     $('#employees-table-body').append(`
         <tr class="${rowClass}">
             <td>${newFirstName}</td>
@@ -65,4 +66,27 @@ function addEmployeeToTable() {
             <td>${salaryNumber}</td>
             <td><button>delete</button></td>
         </tr>`)
+    }
+    
+    /**
+     * hardcoded test 1 test employee
+     */
+
+    // let newLastName = employeeList[0].newLastName;
+    // let newID = employeeList[0].newID;
+    // let newTitle = employeeList[0].newTitle;
+    // let newAnnualSalary = employeeList[0].newAnnualSalary;
+    // let salaryNumber = parseInt(newAnnualSalary);
+    // console.log(newFirstName, newLastName, newID, newTitle, salaryNumber);
+
+    // let rowClass = '';
+    // $('#employees-table-body').append(`
+    //     <tr class="${rowClass}">
+    //         <td>${newFirstName}</td>
+    //         <td>${newLastName}</td>
+    //         <td>${newID}</td>
+    //         <td>${newTitle}</td>
+    //         <td>${salaryNumber}</td>
+    //         <td><button>delete</button></td>
+    //     </tr>`)
 }
